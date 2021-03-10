7bc49c372ec06e3ed21260df4682eafa1212e131
Abigail <abigail.tyrell@noaa.gov>
2021-03-10 14:17:27 -0500

    fix typo

--- R/risk_functions.R
+++ R/risk_functions.R
@@ -361,7 +361,7 @@ plot_risk_by_year <- function(data, indicator, title = "", include_legend) {
   # filter data
   if (indicator[1] != "all") {
     data <- data %>%
-      dplyr::filter(Indicator %in% indicator)
+      dplyr::filter(Indicator %in% indicators)
   }
 
   if (nrow(data) > 0) {
