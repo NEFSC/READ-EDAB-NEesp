32e202b72ec864a5dc5af1fa0c46e463a87d6b8d
Abigail <abigail.tyrell@noaa.gov>
2021-07-12 13:51:30 -0400

    fix typo

--- R/utils.R
+++ R/utils.R
@@ -121,7 +121,7 @@ make_html_table_thin <- function(x, col_names) {
 character_to_factor <- function(x) {
   if (is.null(x) == FALSE) {
     if (nrow(x) > 0) {
-      for (i in seq_len(ncol(data))) {
+      for (i in seq_len(ncol(x))) {
         x <- as.data.frame(x)
         if (class(x[, i]) == "character") {
           x[, i] <- as.factor(x[, i])
